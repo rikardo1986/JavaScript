60158a34d839efa016f0f1b8bd986b1f0cd78ea2
ricardo <rikardocqa21@gmail.com>
2026-05-21 17:58:39 -0400

    ejecricios funciones

---
+++ 07.ejercicios.js
@@ -0,0 +1,4 @@
+// ******************** FUNCIONES ********************
+/* bloque de código reutilizable diseñado para realizar una tarea específica o calcular un valor. 
+Permite encapsular lógica para organizar mejor el programa y evitar repetir el mismo código
+*/
