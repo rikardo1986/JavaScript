89d0b0f0939098b2447668d764f9bc09fd1ce988
ricardo <rikardocqa21@gmail.com>
2026-08-01 20:13:49 -0400

    destructuracion-propagacion2

--- 09.ejercicios.js
+++ 09.ejercicios.js
@@ -5,53 +5,54 @@ Propagación: Es una forma de copiar o combinar arrays u objetos utilizando el o
 Ambas herramientas agilizan drásticamente la manipulación de datos
 */
 
-let miArray = [1, 2, 3, 4];
+let miArray = [1, 2, 3, 4]; // Array de ejemplo
 
 let persona = {
+  // Objeto de ejemplo
   nombre: "Ricardo",
   edad: 40,
   alias: "Rikardo",
 };
 
-let miValor = miArray[1];
+let miValor = miArray[1]; // Acceder al segundo elemento del array
 console.log(miValor);
 
-let myName = persona.nombre;
+let myName = persona.nombre; // Acceder a la propiedad "nombre" del objeto
 console.log(myName);
 
 // Desestructuración
 
 // Sintaxis arrays
 
-let [miValor0, miValor1, miValor2, miValor3, miValor4] = miArray;
-console.log(miValor0);
-console.log(miValor1);
-console.log(miValor2);
-console.log(miValor3);
-console.log(miValor4);
+let [miValor0, miValor1, miValor2, miValor3, miValor4] = miArray; // Desestructuración del array
+console.log(miValor0); // Acceder al primer elemento del array
+console.log(miValor1); // Acceder al segundo elemento del array
+console.log(miValor2); // Acceder al tercer elemento del array
+console.log(miValor3); // Acceder al cuarto elemento del array
+console.log(miValor4); // Acceder al quinto elemento del array
 
 // Sintaxis arrays con valores predeterminados
 
 let [miValue5 = 0, miValue6 = 0, miValue7 = 0, miValue8 = 0, miValue9 = 0] =
-  miArray;
-console.log(miValue5);
-console.log(miValue6);
-console.log(miValue7);
-console.log(miValue8);
-console.log(miValue9);
+  miArray; // Desestructuración del array con valores predeterminados
+console.log(miValue5); // Acceder al primer elemento del array
+console.log(miValue6); // Acceder al segundo elemento del array
+console.log(miValue7); // Acceder al tercer elemento del array
+console.log(miValue8); // Acceder al cuarto elemento del array
+console.log(miValue9); // Acceder al quinto elemento del array
 
 // Ignorar elementos array
 
-let [miValue10, , , miValue13] = miArray;
-console.log(miValue10);
-console.log(miValue13);
+let [miValue10, , , miValue13] = miArray; // Desestructuración del array ignorando elementos
+console.log(miValue10); // Acceder al primer elemento del array
+console.log(miValue13); // Acceder al cuarto elemento del array
 
 // Sintaxis objects
 
-let { nombre, edad, alias } = persona;
-console.log(nombre);
-console.log(edad);
-console.log(alias);
+let { nombre, edad, alias } = persona; // Desestructuración del objeto
+console.log(nombre); // Acceder a la propiedad "nombre" del objeto
+console.log(edad); // Acceder a la propiedad "edad" del objeto
+console.log(alias); // Acceder a la propiedad "alias" del objeto
 
 // Sintaxis objects con valores predeterminados
 
@@ -60,33 +61,37 @@ let {
   edad: edad2,
   alias: alias2,
   email = "email@email.com",
-} = persona;
+} = persona; // Desestructuración del objeto con valores predeterminados
 console.log(nombre2); // No existe
 console.log(edad2); // No existe
 console.log(alias2); // No existe
-console.log(email);
+console.log(email); // Acceder a la propiedad "email" del objeto
 
 // Sintaxis objects con nuevos nombres de variables
 
-let { alias: alias3, nombre: nombre3, edad: edad3 } = persona;
-console.log(nombre3);
-console.log(edad3);
-console.log(alias3);
+let { alias: alias3, nombre: nombre3, edad: edad3 } = persona; // Desestructuración del objeto con nuevos nombres de variables
+console.log(nombre3); // Acceder a la propiedad "nombre" del objeto
+console.log(edad3); // Acceder a la propiedad "edad" del objeto
+console.log(alias3); // Acceder a la propiedad "alias" del objeto
 
 // Objects anidados
 
 let person3 = {
+  // Objeto anidado
   name: "Brais",
   age: 37,
   alias: "MoureDev",
   walk: function () {
-    console.log("La persona camina.");
+    // Método para caminar
+    console.log("La persona camina."); // Imprime un mensaje indicando que la persona camina
   },
   job: {
+    // Objeto anidado que representa el trabajo de la persona
     name: "Programador",
     exp: 15,
     work: function () {
-      console.log(`La persona de ${this.age} años de experiencia trabaja.`);
+      // Método para trabajar
+      console.log(`La persona de ${this.age} años de experiencia trabaja.`); // Imprime un mensaje indicando la experiencia de la persona en su trabajo
     },
   },
 };
@@ -94,42 +99,42 @@ let person3 = {
 let {
   nombre: nombre4,
   job: { name: jobName },
-} = person3;
+} = person3; // Desestructuración del objeto anidado
 
-console.log(nombre4);
-console.log(jobName);
+console.log(nombre4); // Acceder a la propiedad "nombre" del objeto
+console.log(jobName); // Acceder a la propiedad "name" del objeto anidado "job"
 
 // Propagación (...)
 
 // Sintaxis arrays
 
-let myArray2 = [...miArray, 5, 6];
+let myArray2 = [...miArray, 5, 6]; // Propagación del array
 
-console.log(myArray2);
+console.log(myArray2); // Acceder al array propagado
 
 // Copia de arrays
 
-let myArray3 = [...miArray];
+let myArray3 = [...miArray]; // Copia del array
 
-console.log(myArray3);
+console.log(myArray3); // Acceder al array copiado
 
 // Combinación de arrays
 
-let myArray4 = [...miArray, ...myArray2, ...myArray3];
+let myArray4 = [...miArray, ...myArray2, ...myArray3]; // Combinación de arrays
 
-console.log(myArray4);
+console.log(myArray4); // Acceder al array combinado
 
 // Sintaxis objects
 
-let persona4 = { ...persona, email: "braismoure@mouredev.com" };
+let persona4 = { ...persona, email: "braismoure@mouredev.com" }; // Propagación del objeto
 
-console.log(persona4);
+console.log(persona4); // Acceder al objeto propagado
 
 // Copia de objects
 
-let persona5 = { ...persona };
+let persona5 = { ...persona }; // Copia del objeto
 
-console.log(persona5);
+console.log(persona5); // Acceder al objeto copiado
 
 // ******************** EJERCICIOS ********************
 
@@ -217,20 +222,24 @@ console.log(obj4); // { name: "Brais", age: 37 }
 // 10. Combina desestructuración y propagación
 
 let person5 = {
+  // Objeto original
   name: "Brais",
   age: 37,
   alias: "MoureDev",
   walk: function () {
-    console.log("La persona camina.");
+    // Método para caminar
+    console.log("La persona camina."); // Imprime un mensaje indicando que la persona camina
   },
   job: {
+    // Objeto anidado que representa el trabajo de la persona
     name: "Programador",
     exp: 15,
     work: function () {
-      console.log(`La persona de ${this.age} años de experiencia trabaja.`);
+      // Método para trabajar
+      console.log(`La persona de ${this.age} años de experiencia trabaja.`); // Imprime un mensaje indicando la experiencia de la persona en su trabajo
     },
   },
 };
-let { name: name7, ...rest } = person5;
+let { name: name7, ...rest } = person5; // Desestructuración para extraer la propiedad "name" y el resto de las propiedades del objeto
 console.log(name7); // Brais
 console.log(rest); // { age: 37, alias: "MoureDev", walk: [Function], job: { name: "Programador", exp: 15, work: [Function] } }
